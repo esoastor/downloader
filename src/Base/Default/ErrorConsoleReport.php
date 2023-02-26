f7bd7c9ba232b0f83ca43c02115b06f2f5b79047
esoastor <hazehelt@gmail.com>
2023-02-26 13:46:57 +0300

    default console report fixed

--- src/Base/Default/ErrorConsoleReport.php
+++ src/Base/Default/ErrorConsoleReport.php
@@ -5,6 +5,8 @@ class ErrorConsoleReport implements \Downloader\Base\Listener
 {
     public function execute(...$args): void
     {
-        echo "[\e[0;31mX\e[0m] {$args[0]} [{$args[1]}]\n";
+        $message = "[\e[0;31mX\e[0m] {$args[0]}";
+        $message .= isset($args[1]) ? " [{$args[1]}]\n" : "\n"; 
+        echo $message;
     }
 }
