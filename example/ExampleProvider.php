ef7ba54559203456b012a153e9105eb350acd447
esoastor <hazehelt@gmail.com>
2024-01-02 20:24:48 +0300

    provided data deleted

--- example/ExampleProvider.php
+++ example/ExampleProvider.php
@@ -9,6 +9,6 @@ class ExampleProvider implements DownloadInfoProvider
 
     public function provide(): array
     {
-        return ['here' => ['a.png' => 'https://scans-hot.leanbox.us/manga/Chainsaw-Man/0151-001.png', 'b.png' => 'https://scans-hot.leanbox.us/manga/Chainsaw-Man/0151-002.png']];
+        return [];
     }
 }
